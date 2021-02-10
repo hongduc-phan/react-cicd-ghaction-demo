a1cb54c610edcd7be292b6e367c0b22fb11df2a7
PHAN HONG DUC <phduc.phanhongduc@gmail.com>
2021-02-10 14:19:12 +0200

    edit app.js to test autodeploy cicd

--- src/App.js
+++ src/App.js
@@ -5,16 +5,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello World
+          My name is Duc Phan
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
